17365dba46f2db73bd3cf4809b5d4829331ab462
Rattee Wariyawutthiwat <pbserious@gmail.com>
2016-10-18 21:11:56 +0700

    Initial Commit

---
+++ LoginTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginTest
+//
+//  Created by Rattee Wariyawutthiwat on 10/18/2559 BE.
+//  Copyright © 2559 Rattee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
